e5e4c7cd3e1a649a8f235c0490e5106997473b87
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-24 13:16:52 -0900

    new auth assign

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/users.schema';
+import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel: Model<User>,
+        private jwtService: JwtService
+    ){}
+
+    async signUp (signUpDto): Promise<{ token: string }> {
+        const {name, email, password} = signUpDto
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const user = await this.userModel.create({
+            name, 
+            email,
+            password: hashPassword
+        })
+
+        const token = this.jwtService.sign({ id: user._id })
+
+        return {token}
+    }
+}
