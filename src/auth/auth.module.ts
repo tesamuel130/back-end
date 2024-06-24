6cdabd934e198b1e3d2568bd68ab1e8651c53dfe
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-24 12:42:17 -0900

    working on auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserSchema } from './schemas/users.schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-
-    MongooseModule.forFeature([{}])
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.registerAsync({
+      inject: [ConfigService]
+    }),
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
   ],
   controllers: [AuthController],
   providers: [AuthService]
